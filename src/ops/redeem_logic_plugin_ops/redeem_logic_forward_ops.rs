5b449470327104b661e70a4c513f77ffbff1b585
Akshat Mangal <mangalakshat01@gmail.com>
2022-10-28 15:45:37 +0530

    Added functionality to create state for redeem logic forward plugin

--- src/ops/redeem_logic_plugin_ops/redeem_logic_forward_ops.rs
+++ src/ops/redeem_logic_plugin_ops/redeem_logic_forward_ops.rs
@@ -8,15 +8,24 @@ use {
     anchor_client::{
         Program,
         ClientError,
+        solana_sdk:: {
+            signer::keypair::Keypair,
+            signer::Signer,
+            system_program
+        }
     },
-    redeem_logic_forward::RedeemLogicConfig,
+    redeem_logic_forward:: {
+        RedeemLogicConfig,
+        accounts::InitializeContext,
+        instruction::Initialize,
+    },
     crate::utils:: {
         println_name_value,
         println_error
     },
     rust_decimal::{
         Decimal
-    }
+    },
 };
 
 
@@ -27,7 +36,7 @@ pub fn handle_redeem_logic_forward_command(redeem_logic_command: RedeemLogicForw
         RedeemLogicForwardSubcommand::Fetch(fetch_state) => {
             let account:Result<RedeemLogicConfig,ClientError> = program.account(fetch_state.state_id);
             let account = match account {
-                Ok(redeem_config) => (redeem_config),
+                Ok(redeem_config) => redeem_config,
                 Err(err) => {
                     match err {
                         ClientError::AccountNotFound => println_error("Could not find a state with given public key"),
@@ -38,12 +47,42 @@ pub fn handle_redeem_logic_forward_command(redeem_logic_command: RedeemLogicForw
                         ClientError::LogParseError(_)=> println_error("Could not parse the given public key")
                     }
                     exit(1);
-                }
+                }   
             };
             println_name_value("notional", &account.notional);
             println_name_value("is_linear", &account.is_linear);
             println_name_value("strike",&Decimal::deserialize(account.strike));
             println_name_value("owner", &account.owner);
+        },
+        RedeemLogicForwardSubcommand::Create(plugin_state) => {
+            let plugin_config =  Keypair::new();
+            let authority = program.payer();
+            let signature = program.request()
+                .signer(&plugin_config)
+                .accounts(InitializeContext {
+                    redeem_logic_config: plugin_config.pubkey(),
+                    owner: authority,
+                    payer: authority,
+                    system_program: system_program::ID,
+                })
+                .args(Initialize { notional: plugin_state.notional,is_linear:plugin_state.is_linear, strike:plugin_state.strike})
+                .send(); 
+            let signature = match signature {
+                Ok(transaction) => transaction,
+                Err(err) => {
+                    match err {
+                        ClientError::AccountNotFound => println_error("Could not find a state with given public key"),
+                        ClientError::AnchorError(_) => println_error("Anchor not working"),
+                        ClientError::ProgramError(_) => println_error("Redeem Logic Forward program is not working"),
+                        ClientError::SolanaClientError(_) => println_error("Solana client is not working"),
+                        ClientError::SolanaClientPubsubError(_) => println_error("Solana client is not working") ,
+                        ClientError::LogParseError(_)=> println_error("Could not parse the given input")
+                    }
+                    exit(1);
+                }
+            };
+            println_name_value("Redeem Logic Forward State successfully create at", &plugin_config.pubkey());
+            println_name_value("Transaction Id", &signature);                                       
         }
     }
 }
