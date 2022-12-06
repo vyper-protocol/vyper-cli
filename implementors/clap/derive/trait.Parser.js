31d461fdb24e0ba0122884f84653c2d2ad2d640e
thegreat-vanderlinde <thegreat-vanderlinde@users.noreply.github.com>
2022-12-06 13:32:20 +0000

    deploy: 38e71faecc907efd6da702bcfb4532011062d952

--- implementors/clap/derive/trait.Parser.js
+++ implementors/clap/derive/trait.Parser.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["vyper"] = [{"text":"impl Parser for <a class=\"struct\" href=\"vyper/args/config_args/struct.ConfigOptions.html\" title=\"struct vyper::args::config_args::ConfigOptions\">ConfigOptions</a>","synthetic":false,"types":["vyper::args::config_args::ConfigOptions"]},{"text":"impl Parser for <a class=\"struct\" href=\"vyper/args/struct.VyperCliArgs.html\" title=\"struct vyper::args::VyperCliArgs\">VyperCliArgs</a>","synthetic":false,"types":["vyper::args::VyperCliArgs"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"vyper":[["impl Parser for <a class=\"struct\" href=\"vyper/args/config_args/struct.ConfigOptions.html\" title=\"struct vyper::args::config_args::ConfigOptions\">ConfigOptions</a>"],["impl Parser for <a class=\"struct\" href=\"vyper/args/struct.VyperCliArgs.html\" title=\"struct vyper::args::VyperCliArgs\">VyperCliArgs</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["vyper"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vyper/args/config_args/struct.ConfigOptions.html\" title=\"struct vyper::args::config_args::ConfigOptions\">ConfigOptions</a>","synthetic":false,"types":["vyper::args::config_args::ConfigOptions"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"vyper":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"vyper/args/config_args/struct.ConfigOptions.html\" title=\"struct vyper::args::config_args::ConfigOptions\">ConfigOptions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
