1cf08f413506b5948a0bb73c6805058c6645e21f
iamakshat01 <mangalakshat01@gmail.com>
2022-12-12 15:19:07 +0530

    Updated method to fetch otc state

--- src/ops/otc_ops.rs
+++ src/ops/otc_ops.rs
@@ -4,7 +4,9 @@ use {
     crate::utils::{
         println_name_value,
         println_version,
-        println_error
+        println_error,
+        println_date,
+        println_beneficiary_value
     },
     otc_args:: {
         OtcCommand,
@@ -14,7 +16,8 @@ use {
         Program,
         ClientError
     },
-    vyper_otc::state::OtcState
+    vyper_otc::state::OtcState,
+    console::style
 };
 
 
@@ -28,25 +31,25 @@ pub fn handle_otc_command(otc_command: OtcCommand, program: &Program) {
                 Ok(otc_state) => otc_state,
                 Err(err) => {
                     match err {
-                        ClientError::AccountNotFound => println_error("Could not find state of otc with given public key"),
-                        ClientError::AnchorError(_) => println_error("Anchor not working"),
-                        ClientError::ProgramError(_) => println_error("Vyper Otc program is not working"),
-                        ClientError::SolanaClientError(_) => println_error("Solana client is not working"),
-                        ClientError::SolanaClientPubsubError(_) => println_error("Solana client is not working") ,
-                        ClientError::LogParseError(_)=> println_error("Could not parse the given public key")
+                        ClientError::AccountNotFound => println_error("Could not otc state with given public key"),
+                        ClientError::AnchorError(err) => println!("{} : {}",style("error").red().bold(),err),
+                        ClientError::ProgramError(err) => println!("{} : {}",style("error").red().bold(),err),
+                        ClientError::SolanaClientError(err) => println!("{} : {}",style("error").red().bold(),err),
+                        ClientError::SolanaClientPubsubError(err) => println!("{} : {}",style("error").red().bold(),err),
+                        ClientError::LogParseError(err)=> println_error(&err)
                     }
                     exit(1);
                 }
             };
-            println_name_value("created", &account.created);
-            println_name_value("deposit start", &account.deposit_start);
-            println_name_value("deposit end", &account.deposit_end);
-            println_name_value("settle start", &account.settle_start);
+            println_date("created", &account.created);
+            println_date("deposit start", &account.deposit_start);
+            println_date("deposit end", &account.deposit_end);
+            println_date("settle start", &account.settle_start);
             println_name_value("settle executed", &account.settle_executed);
             println_name_value("junior deposit amount", &account.junior_deposit_amount);
             println_name_value("senior deposit amount", &account.senior_deposit_amount);
-            println_name_value("junior side beneficiary", &account.junior_side_beneficiary);
-            println_name_value("senior side beneficiary", &account.senior_side_beneficiary);
+            println_beneficiary_value("junior side beneficiary", &account.junior_side_beneficiary);
+            println_beneficiary_value("senior side beneficiary", &account.senior_side_beneficiary);
             println_name_value("vyper tranche config", &account.vyper_tranche_config);
             println_name_value("vyper core", &account.vyper_core);
             println_name_value("senior reserve token account", &account.otc_senior_reserve_token_account);
@@ -56,7 +59,6 @@ pub fn handle_otc_command(otc_command: OtcCommand, program: &Program) {
             println_name_value("otc authority",&account.otc_authority);
             println_name_value("authority seed", &account.authority_seed);
             println_name_value("authority bump", &account.authority_bump);
-            println_name_value("description", &account.description);
             println_version("version",&account.version);
         }
     }
